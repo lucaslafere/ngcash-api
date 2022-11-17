b42e89df34059f3a306ac9586436b99e8740a96c
Lucas Lafèré <laferelucas@gmail.com>
2022-11-17 16:25:54 -0300

    feat: added get balance route

---
+++ src/routers/accountsRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as accountsController from '../controllers//accountsController';
+import { validateToken } from "../middlewares/validateTokenMiddleware";
+
+const accountsRouter = Router();
+
+accountsRouter.get('/account', validateToken, accountsController.getBalance);
+
+export default accountsRouter
