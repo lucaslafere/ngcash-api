4956822353d86fe2491316d568bae54085574edc
Lucas Lafèré <laferelucas@gmail.com>
2022-11-17 17:25:49 -0300

    fix: cashout service is safer noew

--- src/services/accountsService.ts
+++ src/services/accountsService.ts
@@ -12,14 +12,14 @@ export async function cashOut(username: string, amount: number, userId: number) 
     if (!findAccount) throw {type: 'not-found', message: 'this account doesnt exist'};
     const checkBalance = await findById(findAccount.accountId);
     if (+checkBalance < amount) throw {type: 'unprocessable', message: "you dont have enough balance to conclude this operation"};
-    const newSenderBalance = (+checkBalance - amount);
-    const newBalance = await accountsRepository.updateBalance(findAccount.accountId, newSenderBalance);
-
-
     const findReceiverUser = await usersService.findUserByUsername(username);
     if (!findReceiverUser) throw {type: 'not-found', message: 'this username doesnt exist'};
     const findReceiverAccount = await accountsRepository.findById(findReceiverUser.accountId);
     if (!findReceiverAccount) throw {type: 'not-found', message: 'this account doesnt exist'};
+    
+    const newSenderBalance = (+checkBalance - amount);
+    const newBalance = await accountsRepository.updateBalance(findAccount.accountId, newSenderBalance);
+
     const receiverBalance = await findById(findReceiverAccount.id);
     const newReceiverBalance = (+receiverBalance + amount);
     await accountsRepository.updateBalance(findReceiverAccount.id, newReceiverBalance)
