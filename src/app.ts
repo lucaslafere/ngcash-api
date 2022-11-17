9bdae7eae8d8866f4ad8ce094d73c2df7f9f2542
Lucas Lafèré <laferelucas@gmail.com>
2022-11-17 12:25:59 -0300

    feat: added router to app

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,12 @@ import cors from 'cors';
 import express, { json } from 'express';
 import dotenv from 'dotenv';
 import "express-async-errors";
+import router from './routers/indexRouter'
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(json());
-
+app.use(router);
 
 export default app;
