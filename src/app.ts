ddde3ecbb08bb053140534a3c0d758d23699ba32
Lucas Lafèré <laferelucas@gmail.com>
2022-11-17 14:59:35 -0300

    feat: added error handler middleware to app

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import cors from 'cors';
 import express, { json } from 'express';
 import dotenv from 'dotenv';
 import "express-async-errors";
+import errorHandlerMiddleware from './middlewares/errorHandlerMiddleware';
 import router from './routers/indexRouter'
 
 dotenv.config();
@@ -10,4 +11,5 @@ app.use(cors());
 app.use(json());
 app.use(router);
 
+app.use(errorHandlerMiddleware);
 export default app;
