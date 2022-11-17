0ea771e214e11f5a714505d2c39e51485c31ca6d
Lucas Lafèré <laferelucas@gmail.com>
2022-11-17 14:57:34 -0300

    feat: added userdata type

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,3 +1,6 @@
 import { Request, Response } from 'express';
 import { createUserSchema } from '../schemas/usersSchemas';
 import { signInSchema } from '../schemas/authenticationSchemas';
+import * as manipulateToken from '../utils/manipulateToken';
+import { UserData } from '../types/userType';
+
