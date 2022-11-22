59ae8d38a0c49a94e9567c4cb952963ef228d756
Lucas Lafèré <laferelucas@gmail.com>
2022-11-22 11:33:12 -0300

    Update accountsRouter.ts

--- src/routers/accountsRouter.ts
+++ src/routers/accountsRouter.ts
@@ -5,10 +5,10 @@ import { validateToken } from "../middlewares/validateTokenMiddleware";
 const accountsRouter = Router();
 
 accountsRouter.get('/account', validateToken, accountsController.getBalance);
-accountsRouter.put('/cash-out', validateToken, accountsController.cashOut)
-accountsRouter.get('/transactions-old', validateToken, accountsController.getUserTransactionsAscending)
-accountsRouter.get('/transactions-new', validateToken, accountsController.getUserTransactionsDescending)
-accountsRouter.get('/cash-out', validateToken, accountsController.getUserCashOut)
-accountsRouter.get('/cash-in', validateToken, accountsController.getUserCashIn)
+accountsRouter.put('/transactions/cash-out', validateToken, accountsController.cashOut)
+accountsRouter.get('/transactions/old', validateToken, accountsController.getUserTransactionsAscending)
+accountsRouter.get('/transactions/new', validateToken, accountsController.getUserTransactionsDescending)
+accountsRouter.get('/transactions/cash-out', validateToken, accountsController.getUserCashOut)
+accountsRouter.get('/transactions/cash-in', validateToken, accountsController.getUserCashIn)
 
 export default accountsRouter
